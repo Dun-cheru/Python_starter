abf9a4b9bed7a9f5e2a5c04e0895d15cc43a3032
Dunnieboy <duncanlinnaeus98@gmail.com>
2020-01-21 18:10:50 +0000

    Functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+"""
+Function is a collection of code which performs a specific task
+"""
+#d ef is used to describe that this is a code
+# Now we call the function in order to do that we type the functions name
+def say_hi(name, age, study, work, occupation):
+    print("Hello " + name + " You are ! " + str(age) + ", years old "
+          + " You studied at" + study + ", you work at " + work + " Currently " + occupation)
+say_hi("Dee", 22, "The Technical University of Kenya", "westlands", "Cuffed")
+say_hi("Gee", 18, "Qatar School of Aviation", "Kenya airways", "cuffed")
+
+def say_hi(name, age, work,study):
+    print("Hello " + name + ", you are ! " + str(age) + ", You work in! " +
+          work + ", Studied at " + study)
+say_hi("Dunnieboy", 22, "Westlands", "The Technical University of Kenya")
+say_hi("Geestar", 18, "Kenya airways", "Qatar School of Aviation")
+
+#Do revision here Dee !!You are Terrible
