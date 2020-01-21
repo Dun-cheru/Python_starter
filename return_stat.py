41c284226c9c738e43d9c2f66391739c19f31b14
Dunnieboy <duncanlinnaeus98@gmail.com>
2020-01-21 18:13:27 +0000

    return statements

---
+++ return_stat.py
@@ -0,0 +1,12 @@
+
+def cube(num):
+    return num*num*num
+
+result = cube(4)
+print(result)
+
+def square(num):
+    return num*num
+result = square(6)
+print(result)
+# NB after the return statement you cannot add another code  because it breaks the function
