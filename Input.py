e724a06619e8855cb41b70ff197ec8614d47295d
Dunnieboy <duncanlinnaeus98@gmail.com>
2020-01-21 18:09:13 +0000

    user_input

---
+++ Input.py
@@ -0,0 +1,14 @@
+"""
+Getting input from users
+"""
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+phone = input("Enter your phone number: 254")
+country = input("Country: ")
+email = input("Enter your email: ")
+work = input("Where did you work: ")
+study = input("Where do you study: ")
+print("Hello " + name  + "! You are " + age  + " Your phone number is "
+    + phone + " You live in " + country + " Your Email is " + email +
+      " You work in " + work + " you studied at! " + study)
+
